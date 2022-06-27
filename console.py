869c623da451f03f72a5f8be746a350613fe7961
Karoline-S <karoline.amy@gmai.com>
2022-06-27 00:20:59 -0700

    build - work on console.py

--- console.py
+++ console.py
@@ -3,13 +3,24 @@
 The entry point for the command intepreter
 """
 
-import cmd.Cmd
+import cmd
 
 class HBNBCommand(cmd.Cmd):
     """
-    an interpreter class inheriting from Cmd
+    an interpreter class inheriting from cmd
     """
-    def __init__(self):
+    prompt = '(hbnb) '
+
+    def do_test(self, line):
+        print("testing testing")
+
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, line):
+        return self.do_EOF
+
+    def help_quit(self):
         pass
 
 if __name__ == '__main__':
