bb93ed4c46ed333f0f9d2c726a7b17419204852f
Karoline-S <karoline.amy@gmai.com>
2022-06-27 22:46:35 -0700

    add intro message

--- console.py
+++ console.py
@@ -4,12 +4,14 @@ The entry point for the command intepreter
 """
 
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
     an interpreter class inheriting from cmd
     """
     prompt = '(hbnb) '
+    class_list = ["BaseModel"]
 
     def do_EOF(self, line):
         """quits the intepreter"""
@@ -19,5 +21,16 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
         return self.do_EOF
 
+    def do_create(self, line):
+        """creates a new instance"""
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        if line not in cls.class_list:
+            print("** class doesn't exist **")
+            return
+        new_obj = line[0]()
+        print(new_obj.id)
+
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop(intro="Python stinks")
