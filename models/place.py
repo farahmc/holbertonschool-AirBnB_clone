6655618022c4feac3d4d00f257a8c255d59c1899
Farah <farah.mccurdy@gmail.com>
2022-06-27 22:45:03 -0700

    add: file

---
+++ models/place.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" Module for class Place """
+
+
+class Place(BaseModel):
+    """
+    Class Place that imports from BaseModel
+
+    Attributes:
+    city_id: string - empty string: it will be the City.id
+    user_id: string - empty string: it will be the User.id
+    name: string - empty string
+    description: string - empty string
+    number_rooms: integer - 0
+    number_bathrooms: integer - 0
+    max_guest: integer - 0
+    price_by_night: integer - 0
+    latitude: float - 0.0
+    longitude: float - 0.0
+    amenity_ids: list of string - empty list: it will be the list of Amenity.id later
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = int(0)
+    number_bathrooms = int(0)
+    max_guest = int(0)
+    price_by_night = int(0)
+    latitude = float(0.0)
+    longitude = float(0.0)
+    amenity_ids = []
