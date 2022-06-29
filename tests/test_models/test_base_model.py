fec41af7cb4b68894c41ac1555bace1cbd2b3806
Farah <farah.mccurdy@gmail.com>
2022-06-28 22:06:13 -0700

    add: more instantiate tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,11 +25,39 @@ class TestBaseModel(unittest.TestCase):
         """ Happy pass updated at datetime """
         self.assertEqual(datetime, type(BaseModel().updated_at))
 
+    def test_str_out(self):
+        """ Happy pass string representation """
+        base1 = BaseModel()
+        str_out = f"[{base1.__class__.__name__}] ({base1.id} {base1.__dict__})"
+        self.assertEqual(str_out, str(base1))
+
     def test_uid(self):
-        """ Test uid created at each instantiation """
+        """ UID created at each instantiation """
         base1 = BaseModel()
         base2 = BaseModel()
         self.assertNotEqual(base1.id, base2.id)
+
+    def test_instantiate_attrs(self):
+        """ Single instantiate and check attributes """
+        base1 = BaseModel()
+        self.assertEqual(type(base1.id), str)
+        self.assertEqual(type(base1.created_at), datetime)
+        self.assertEqual(type(base1.updated_at), datetime)
+
+    def test_instantiate_kwargs(self):
+        """ Single instantiate with kwargs """
+        dt = datetime.today()
+        base1 = BaseModel(
+            id="123", created_at=dt.isoformat(), updated_at=dt.isoformat()
+        )
+        self.assertEqual(base1.id, "123")
+        self.assertEqual(base1.created_at, dt)
+        self.assertEqual(base1.updated_at, dt)
+
+    def test_str(self):
+        """ ___str__ method is string """
+        base1 = BaseModel()
+        self.assertEqual(type(str(base1)), str)
 
 
 if __name__ == "__main__":
