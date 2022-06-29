53fa349215a950f17d50adda5bd7ec4b3007d852
jql11 <4325@holbertonschool.com>
2022-06-29 01:50:29 -0700

    add file

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -8,7 +8,7 @@ import json
 import os
 import pycodestyle
 from models.base_model import BaseModel
-from models.state import State
+from models.city import City
 
 
 class TestFileStorageDocs(unittest.TestCase):
@@ -17,12 +17,12 @@ class TestFileStorageDocs(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
-        cls.state_funcs = inspect.getmembers(State, inspect.isfunction)
+        cls.city_funcs = inspect.getmembers(City, inspect.isfunction)
 
     def test_conformance_class(self):
         """Test that we conform to Pycodestyle."""
         style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/state.py'])
+        result = style.check_files(['models/city.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
@@ -30,51 +30,51 @@ class TestFileStorageDocs(unittest.TestCase):
         """Test that we conform to Pycodestyle."""
         style = pycodestyle.StyleGuide(quiet=True)
         result = style.\
-            check_files(['tests/test_models/test_state.py'])
+            check_files(['tests/test_models/test_city.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_module_docstr(self):
         """ Tests for docstring"""
-        self.assertTrue(len(State.__doc__) >= 1)
+        self.assertTrue(len(City.__doc__) >= 1)
 
     def test_class_docstr(self):
         """ Tests for docstring"""
-        self.assertTrue(len(State.__doc__) >= 1)
+        self.assertTrue(len(City.__doc__) >= 1)
 
     def test_func_docstr(self):
         """Tests for docstrings in all functions"""
-        for func in self.state_funcs:
+        for func in self.city_funcs:
             self.assertTrue(len(func[1].__doc__) >= 1)
 
 
 class TestState(unittest.TestCase):
 
     def test_is_subclass(self):
-        self.assertTrue(issubclass(State().__class__, BaseModel), True)
+        self.assertTrue(issubclass(City().__class__, BaseModel), True)
 
     def test_attr_str(self):
-        self.assertEqual(type(State().name), str)
+        self.assertEqual(type(City().name), str)
+        self.assertEqual(type(City().state_id), str)
 
-    def test_has_attributes(self):
-        Victoria = State()
-        Victoria.name = "Victoria"
-        self.assertTrue('id' in Victoria.to_dict())
-        self.assertTrue('created_at' in Victoria.to_dict())
-        self.assertTrue('updated_at' in Victoria.to_dict())
-        self.assertTrue('name' in Victoria.to_dict())
+    def test_has_attributes_in_to_dict(self):
+        """check if attr is in to_dict"""
+        melbourne = City()
+        melbourne.name = "Melbourne"
+        melbourne.state_id = "vic"
+        self.assertTrue('id' in melbourne.to_dict())
+        self.assertTrue('created_at' in melbourne.to_dict())
+        self.assertTrue('updated_at' in melbourne.to_dict())
+        self.assertTrue('name' in melbourne.to_dict())
+        self.assertTrue('state_id' in melbourne.to_dict())
 
     def test_save(self):
-        Victoria = State()
-        Victoria.name = "Victoria"
-        Victoria.save()
-        self.assertNotEqual(Victoria.created_at, Victoria.updated_at)
+        melbourne = City()
+        melbourne.name = "Melbourne"
+        melbourne.save()
+        self.assertNotEqual(melbourne.created_at, melbourne.updated_at)
 
     def test_to_dict(self):
-        Victoria = State()
-        self.assertTrue(dict, type(Victoria.to_dict))
-        self.assertEqual('to_dict' in dir(Victoria), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        melbourne = City()
+        self.assertTrue(dict, type(melbourne.to_dict))
+        self.assertEqual('to_dict' in dir(melbourne), True)
